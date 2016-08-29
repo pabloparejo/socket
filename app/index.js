bde77cd835178548a9e2f34f9fc7fea4a0924327
pabloparejo <parejo.p@gmail.com>
2016-08-29 15:32:16 +0200

    JWT tutorial first steps

--- app/index.js
+++ app/index.js
@@ -1,20 +1,46 @@
 const express = require("express")
 const bodyParser= require('body-parser')
 const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('static/public'));
+
+const morgan = require('morgan');
+const jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const config = require('./config'); // get our config file
 
 const http = require("http").Server(app)
 const io = require("socket.io")(http)
 
 const db = require("./db.js")
-const api = require("./apiRoutes.js")(app, express.Router(), db)
+const api = require("./apiRoutes")(app, express.Router(), db)
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('static/public'));
+app.set('secret', config.secret); // secret variable
 
 app.get("/", function (req, res) {
     var route = __dirname.replace("app", "static/index.html")
     res.sendFile(route)
 })
+
+app.get('/setup', function(req, res) {
+    var User = db.models.User
+    // create a sample user
+    var rand = Math.random()
+    var nick = new User({ 
+        username: rand, 
+        password: 'password',
+        fullname: "filemoncho" 
+    });
+
+    // save the sample user
+    nick.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
+});
 
 
 http.listen(3000, function(){
